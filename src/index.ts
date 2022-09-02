140164e160ba23fb3aea4bcd2e48748c784310df
Thales Alves <thalesalves758@gmail.com>
2022-09-02 01:02:08 -0300

    feat: use errorHanlder

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes/index.routes';
+import errorHandler from './middlewares/errorHandler.middleware';
 
 dotenv.config();
 
@@ -10,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000; /* eslint-disable-line */
 
