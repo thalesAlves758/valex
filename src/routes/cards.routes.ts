030c1cfb7cd0c8d6ca196785d185b5088207134c
Thales Alves <thalesalves758@gmail.com>
2022-09-02 11:20:34 -0300

    feat: use card schema

--- src/routes/cards.routes.ts
+++ src/routes/cards.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import * as cardController from '../controllers/card.controller';
 import validateApiKey from '../middlewares/validateApiKey.middleware';
+import validateSchema from '../middlewares/validateSchema.middleware';
+import cardSchema from '../schemas/card.schema';
 
 const cardsRouter = Router();
 
-cardsRouter.post('/cards', validateApiKey, cardController.create);
+cardsRouter.post(
+  '/cards',
+  validateApiKey,
+  validateSchema(cardSchema),
+  cardController.create
+);
 
 export default cardsRouter;
