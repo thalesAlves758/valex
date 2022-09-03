98756fb23be048b6c651a543cfb2440d4d7fdbb4
Thales Alves <thalesalves758@gmail.com>
2022-09-02 21:54:37 -0300

    create POST /cards/:cardId/active route

--- src/routes/cards.routes.ts
+++ src/routes/cards.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import * as cardController from '../controllers/card.controller';
 import validateApiKey from '../middlewares/validateApiKey.middleware';
 import validateSchema from '../middlewares/validateSchema.middleware';
+import activeCardSchema from '../schemas/activeCard.schema';
 import cardSchema from '../schemas/card.schema';
 
 const cardsRouter = Router();
@@ -11,6 +12,11 @@ cardsRouter.post(
   validateApiKey,
   validateSchema(cardSchema),
   cardController.create
+);
+cardsRouter.post(
+  '/cards/:cardId/active',
+  validateSchema(activeCardSchema),
+  cardController.active
 );
 
 export default cardsRouter;
