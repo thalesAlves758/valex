bfd665eb9fc1d4dfe0e70aff583556b5218fb3ab
Thales Alves <thalesalves758@gmail.com>
2022-09-03 12:32:43 -0300

    feat: create recharge card function

--- src/services/recharge.services.ts
+++ src/services/recharge.services.ts
@@ -1,5 +1,23 @@
-import { findByCardId, Recharge } from '../repositories/rechargeRepository';
+import {
+  findByCardId,
+  insert,
+  Recharge,
+} from '../repositories/rechargeRepository';
+import {
+  getCardById,
+  validateCardExpiration,
+  validateNoActivedCard,
+} from './card.services';
 
 export async function getRechargeByCardId(cardId: number): Promise<Recharge[]> {
   return findByCardId(cardId);
+}
+
+export async function rechargeCardByid(cardId: number, amount: number) {
+  const card = await getCardById(cardId);
+
+  validateNoActivedCard(card.password);
+  validateCardExpiration(card.expirationDate);
+
+  await insert({ cardId, amount });
 }
