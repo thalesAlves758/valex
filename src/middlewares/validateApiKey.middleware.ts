a85435c39dcd304a8667eca4dd553841f7bd1a2f
Thales Alves <thalesalves758@gmail.com>
2022-09-03 12:14:17 -0300

    feat: send company to controller by res.locals

--- src/middlewares/validateApiKey.middleware.ts
+++ src/middlewares/validateApiKey.middleware.ts
@@ -1,13 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
+import { findCompanyByApiKey } from '../services/company.services';
 import httpStatus from '../utils/HttpStatus';
 
-function validateApiKey(req: Request, res: Response, next: NextFunction) {
+async function validateApiKey(req: Request, res: Response, next: NextFunction) {
   const apiKey: string | undefined = req.get('x-api-key');
 
   if (!apiKey) {
     res.sendStatus(httpStatus.UNAUTHORIZED);
     return;
   }
+
+  const company = await findCompanyByApiKey(apiKey);
+
+  res.locals = { company };
 
   next();
 }
