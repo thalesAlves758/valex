be7525f4c32a9d7b6306d7459f6ce6116be27cce
Thales Alves <thalesalves758@gmail.com>
2022-09-05 15:42:32 -0300

    feat: force security code pattern

--- src/schemas/activeCard.schema.ts
+++ src/schemas/activeCard.schema.ts
@@ -2,9 +2,13 @@ import Joi from 'joi';
 import getPasswordTypeValidation from '../utils/passwordTypeValidation';
 
 const SECURITY_CODE_LENGTH = 3;
+const securityCodeRegex = /^[0-9]{3}$/;
 
 const activeCardSchema = Joi.object({
-  securityCode: Joi.string().length(SECURITY_CODE_LENGTH).required(),
+  securityCode: Joi.string()
+    .regex(securityCodeRegex)
+    .length(SECURITY_CODE_LENGTH)
+    .required(),
   password: getPasswordTypeValidation(),
 });
 
